119233ae88f5b7a932d0d9d26e12702e8e596981
cedis81 <ccchom11@gmail.com>
2020-12-22 15:36:40 -0500

    Create profile route
    * Create profile route on server.js
    * Success returned from Postman API call

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 const database = {
   users: [
     {
-      id: '123,',
+      id: '123',
       name: 'john',
       email: 'john@gmail.com',
       password: 'cookies',
@@ -16,7 +16,7 @@ const database = {
       joined: new Date()
     },
     {
-      id: '124,',
+      id: '124',
       name: 'sally',
       email: 'sally@gmail.com',
       password: 'bananas',
@@ -42,7 +42,7 @@ app.post('/signin', (req, res) => {
 app.post('/register', (req, res) => {
   const { email, name, password } = req.body;
   database.users.push({
-    id: '125,',
+    id: '125',
     name: name,
     email: email,
     password: password,
@@ -50,6 +50,20 @@ app.post('/register', (req, res) => {
     joined: new Date()
   })
   res.json(database.users[database.users.length-1]);
+})
+
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id == id) {
+      found = true;
+      return res.json(user);
+    }
+  })
+  if (!found) {
+    res.status(400).json('not found');
+  }
 })
 
 app.listen(3000, ()=>{
