6d236dd8e3863a3e390c5624ecf5fa4cf69ee8de
cedis81 <ccchom11@gmail.com>
2021-01-25 16:37:22 -0500

    Require knex
    * Remove previous static database
    * Import knex
    * Create database function for knex use

--- server.js
+++ server.js
@@ -2,32 +2,24 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex');
+
+const database = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    // user : '',
+    // password : '',
+    database : 'smart-brain'
+  }
+});
+
+database.select('*').from('users');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
-
-const database = {
-  users: [
-    {
-      id: '123',
-      name: 'john',
-      email: 'john@gmail.com',
-      password: 'cookies',
-      entries: 0,
-      joined: new Date()
-    },
-    {
-      id: '124',
-      name: 'sally',
-      email: 'sally@gmail.com',
-      password: 'bananas',
-      entries: 0,
-      joined: new Date()
-    }
-  ]
-}
 
 app.get('/', (req, res) => {
   res.send(database.users)
