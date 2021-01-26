96650146a518c6168fac484907f8081083fc39ad
cedis81 <ccchom11@gmail.com>
2021-01-26 12:54:33 -0500

    Signin route updated
    * Remove static database functions
    * Use knex function to compare password to hash from login table
    * Return user if credentials match, error message if they do not

--- server.js
+++ server.js
@@ -24,23 +24,26 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', (req, res) => {
-  bcrypt.compare(req.body.password, '$2a$10$9Z9UGD8MUz8cyIoJOOWcjum0Fetw05Qm9Fiyd8C5X6gEQ98pep3jC', function(err, res) {
-    console.log('sign in guess', res)
-  });
-  if (req.body.email === database.users[0].email &&
-      req.body.password === database.users[0].password) {
-    res.json(database.users[0]);
-  } else {
-    res.status(400).json('error logging in')
-  }
+  const { email, name, password } = req.body;
+  database.select('email', 'hash').from('login')
+    .where('email', '=', email)
+    .then(response => {
+      const isValid = bcrypt.compareSync(password, response[0].hash);
+      if(isValid) {
+        return database.select('*').from('users').where('email', '=', email)
+        .then(user => {
+          res.json(user[0])
+        })
+        .catch(err => res.status(400).json('unable to sign in'))
+      } else {
+        res.status(400).json('Wrong credentials.')
+      }
+    })
+    .catch(err => res.status(400).json('Wrong credentials.'))
 })
 
 app.post('/register', (req, res) => {
   const { email, name, password } = req.body;
-  // bcrypt.hash(password, null, null, function(err, hash) {
-  //   console.log(err);
-  //   console.log(hash);
-  // });
   const hash = bcrypt.hashSync(password);
   database.transaction(trx => {
     trx.insert({
@@ -88,14 +91,6 @@ app.put('/image', (req, res) => {
     .then(entries => res.json(entries[0]))
     .catch(err => res.status(400).json('Unable to get entries.'))
 })
-
-// // Load hash from your password DB.
-// bcrypt.compare("bacon", hash, function(err, res) {
-//     // res == true
-// });
-// bcrypt.compare("veggies", hash, function(err, res) {
-//     // res = false
-// });
 
 app.listen(3000, ()=>{
   console.log('app is running on port 3000')
