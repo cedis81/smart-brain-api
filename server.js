069b0276a4c91f68958a661a127b9b8d846032dd
cedis81 <ccchom11@gmail.com>
2021-01-26 12:00:44 -0500

    Get route individual profile updated and complete
    * Remove found variable and related functions to static database
    * Use knex function to return user by userid
    * Returns user if found, error message if not found

--- server.js
+++ server.js
@@ -44,24 +44,23 @@ app.post('/register', (req, res) => {
     name: name,
     joined: new Date()
   })
-  .then(user => {
-    res.json(user[0])
-  })
-  .catch(err => res.status(400).json('Unable to register.'))
+    .then(user => {
+      res.json(user[0])
+    })
+    .catch(err => res.status(400).json('Unable to register.'))
 })
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
-  let found = false;
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      return res.json(user);
-    }
-  })
-  if (!found) {
-    res.status(400).json('not found');
-  }
+  database.select('*').from('users').where({id})
+    .then(user => {
+      if(user.length) {
+        res.json(user[0])
+      } else {
+        res.status(400).json('Not found.')
+      }
+    })
+    .catch(err => res.status(400).json('Error retrieving user.'))
 })
 
 app.put('/image', (req, res) => {
