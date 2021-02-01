508de3379033da7deef7cd0e4f4abfedfab5a80a
cedis81 <ccchom11@gmail.com>
2021-02-01 16:03:55 -0500

    Move register route to new file
    * Create controllers directory
    * Create register.js in controllers directory
    * Move register function to register.js in controllers directory
    * Require register directory in server.js
    * Inject database and bcrypt dependencies in register route in server.js

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 
+const register = require('./controllers/register');
+
 const database = knex({
   client: 'pg',
   connection: {
@@ -42,33 +44,7 @@ app.post('/signin', (req, res) => {
     .catch(err => res.status(400).json('Wrong credentials.'))
 })
 
-app.post('/register', (req, res) => {
-  const { email, name, password } = req.body;
-  const hash = bcrypt.hashSync(password);
-  database.transaction(trx => {
-    trx.insert({
-      hash: hash,
-      email: email
-    })
-    .into('login')
-    .returning('email')
-    .then(loginEmail => {
-      return trx('users')
-        .returning('*')
-        .insert({
-          email: loginEmail[0],
-          name: name,
-          joined: new Date()
-        })
-        .then(user => {
-          res.json(user[0])
-        })
-    })
-    .then(trx.commit)
-    .catch(trx.rollback)
-  })
-  .catch(err => res.status(400).json('Unable to register.'))
-})
+app.post('/register', (req, res) => { register.handleRegister(req, res, database, bcrypt) })
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
