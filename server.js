491431328dc203c6910f50eb5f35bcd8d54f09f2
cedis81 <ccchom11@gmail.com>
2021-02-08 15:04:50 -0500

    Change ssl authorization

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ const database = knex({
   client: 'pg',
   connection: {
     connectionString: process.env.DATABASE_URL,
-    ssl: true,
+    ssl: {
+      rejectUnauthorized: false
+    }
   }
 });
 
